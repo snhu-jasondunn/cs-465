a84e73046d382ddd3285b1b465e95f99b03d3d75
Jason Dunn <jason.dunn2@snhu.edu>
2026-05-25 19:46:38 -0700

    Module 5 completed baseline

---
+++ app_api/controllers/trips.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const Trip = require("../models/travlr");
+const Model = mongoose.model("trips");
+
+// GET: /trips - list all the trips
+// regardless of outcome, response must include HTML status code and JSON message to the requesting client
+const tripsList = async (req, res) => {
+  const q = await Model.find({}).exec(); // no filter, return all records
+  
+  //console.log(q);
+
+  if (!q) {
+    return res.status(404).json(err);
+  } else {
+    return res.status(200).json(q);
+  }
+};
+
+const tripsFindByCode = async (req, res) => {
+  const q = await Model.find({'code': req.params.tripCode}).exec(); // no filter, return all records
+  
+  //console.log(q);
+
+  if (!q) {
+    return res.status(404).json(err);
+  } else {
+    return res.status(200).json(q);
+  }
+};
+
+module.exports = {
+  tripsList,
+  tripsFindByCode
+};
--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,12 +1,39 @@
-var fs = require('fs');
-var trips = JSON.parse(fs.readFileSync('./data/trips.json','utf8'));
-
+//var fs = require("fs");
+//var trips = JSON.parse(fs.readFileSync('./data/trips.json','utf8'));
 
 /* GET Travel */
-const travel = (req, res) => {
-    res.render('travel', {title: "Travlr Getaways", trips});
+/* const travel = (req, res,) => {
+  res.render("travel", { title: "Travlr Getaways", trips });
+}; */
+
+const tripsEndpoint = "http://localhost:3000/api/trips";
+const options = {
+  method: "GET",
+  headers: {
+    Accept: "application/json",
+  },
 };
 
-module.exports={
-    travel
-}
+const travel = async function (req, res, next) {
+  console.log("start");
+  await fetch(tripsEndpoint, options)
+    .then(res => res.json())
+    .then(json => {
+      //console.log(json);
+      let message = null;
+      if (!(json instanceof Array)){
+        message= "API Lookup error";
+        json=[];
+      }else{
+        if(!json.length){
+            message="No trips exist in our database!";
+        }
+      }
+      res.render("travel", { title: "Travlr Getaways", trips: json, message});
+    })
+    .catch(err => res.status(500).send(err.message));
+};
+
+module.exports = {
+  travel,
+};
